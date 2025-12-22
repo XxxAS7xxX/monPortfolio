2ea0a7af2be9293f00e050ae1ebd4f622bf8d004
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-22 17:41:14 +0000

    accueil

--- src/accueil.jsx
+++ src/accueil.jsx
@@ -1,7 +1,11 @@
+import './styles/accueil.css';
+
 const Accueil = () => (
     <div className="accueil">
-        <h1>Bienvenue sur mon portfolio</h1>
-        <p>Explorez mes projets et compétences en développement web.</p>
+        <div className="haut">
+            <h1>Bienvenue sur mon portfolio</h1>
+            <p>Explorez mes projets et compétences en développement web.</p>
+        </div>
     </div>
 );
 
