363a521de880648fbe1dd3945ec5788579242b4a
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-23 19:37:32 +0000

    debut projets

--- src/projets.jsx
+++ src/projets.jsx
@@ -1,7 +1,7 @@
-
+import './styles/projets.css';
 function Projets() {
   return (
-    <div>
+    <div className="projets">
       <h1>Mes Projets</h1>
       <p>Liste de mes projets...</p>
     </div>
