3d333376e2bf22c73da8b93f1aec2989ce3faf42
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-24 13:29:23 +0000

    projets

--- src/projets.jsx
+++ src/projets.jsx
@@ -3,7 +3,32 @@ function Projets() {
   return (
     <div className="projets">
       <h1>Mes Projets</h1>
-      <p>Liste de mes projets...</p>
+      <table>
+        <tbody>
+          <tr>
+            <div className="container">
+              <div className="card">
+                <td>Projet Magic Hanout</td>
+                <td>Site de vente en ligne, de produits fictifs magiques</td>
+                <td>Symfony</td>
+                <td><a href="https://github.com/btssiojvpromo2025-2026/ppe3-microsoft-support-indian-not-scam.git">Aller voir</a></td>
+              </div>
+              <div className="card">
+                <td>Extention Chrome</td>
+                <td>Prise en main des outils de developpement chrome, afin de créer une extension</td>
+                <td>Json, environnement chrome</td>
+                <td><a href="https://github.com/XxxAS7xxX/chromeFH.git">Aller voir</a></td>
+              </div>
+              <div className="card">
+                <td>Projet Gestionnaire de Musique</td>
+                <td>Site de gestion de musique, à partir d'une base de données</td>
+                <td>Symfony</td>
+                <td><a href="https://github.com/btssiojvpromo2025-2026/gestionmusique-XxxAS7xxX.git">Aller voir</a></td>
+              </div>
+            </div>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
