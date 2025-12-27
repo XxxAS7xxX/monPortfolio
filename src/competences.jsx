21d2cd5dcbad4a2cd2ebe205d2691d7444746802
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-27 22:44:40 +0000

    ajout logo1

--- src/competences.jsx
+++ src/competences.jsx
@@ -1,6 +1,7 @@
 import styles from './styles/competences.module.css';
 import js from './assets/js.png';
 import reactLogo from './assets/react.png';
+import php from './assets/php.png';
 
 function Competences() {
   return (
@@ -9,6 +10,7 @@ function Competences() {
       <p>Technologies maîtrisées...</p>
       <img src={js} alt="JavaScript" />
       <img src={reactLogo} alt="React" />
+      <img src={php} alt="PHP" />
     </div>
   )
 }
