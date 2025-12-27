9b3264d5d60b006d90f86ea0ae242a94dab76ce2
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-27 22:41:38 +0000

    ajout logo

--- src/competences.jsx
+++ src/competences.jsx
@@ -1,10 +1,14 @@
 import styles from './styles/competences.module.css';
+import js from './assets/js.png';
+import reactLogo from './assets/react.png';
 
 function Competences() {
   return (
     <div className={styles.competences}>
       <h1>Mes Compétences</h1>
       <p>Technologies maîtrisées...</p>
+      <img src={js} alt="JavaScript" />
+      <img src={reactLogo} alt="React" />
     </div>
   )
 }
