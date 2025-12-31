e7ad1dfe2758473266d8c760cb45a66bc0fa4fad
XxxAS7xxX <axel.delpierre78@gmail.com>
2025-12-31 13:54:34 +0000

    outils manque css

--- src/competences.jsx
+++ src/competences.jsx
@@ -4,6 +4,9 @@ import reactLogo from './assets/react.png';
 import php from './assets/php.png';
 import python from './assets/python.png';
 import csharp from './assets/Csharp.png';
+import html5 from './assets/html5.png';
+import css from './assets/css.png';
+import sql from './assets/sql.png';
 import symfony from './assets/symfony.png';
 import mui from './assets/mui.png';
 import bootstrap from './assets/bootstrap.png';
@@ -11,6 +14,10 @@ import windows from './assets/windows.png';
 import linux from './assets/linux.png';
 import ubuntu from './assets/ubuntu.png';
 import macos from './assets/macos.png';
+import vscode from './assets/vscode.png';
+import postman from './assets/postman.png';
+import github from './assets/github.png';
+import cisco from './assets/cisco.png';
 import useOverflow from './useOverflow';
 
 
@@ -24,20 +31,32 @@ function Competences() {
         <h2>Langages de Programmation</h2>
         <div className={styles.logos}>
           <div className={styles.logo}>
-            <img src={js} alt="JavaScript Logo" />
-            <p>JavaScript</p>
+            <img src={html5} alt="HTML5 Logo" />
+            <p>HTML5</p>
           </div>
           <div className={styles.logo}>
-            <img src={python} alt="Python Logo" />
-            <p>Python</p>
+            <img src={css} alt="CSS Logo" />
+            <p>CSS</p>
+          </div>
+          <div className={styles.logo}>
+            <img src={js} alt="JavaScript Logo" />
+            <p>JavaScript</p>
           </div>
           <div className={styles.logo}>
             <img src={php} alt="PHP Logo" />
             <p>PHP</p>
           </div>
           <div className={styles.logo}>
+            <img src={python} alt="Python Logo" />
+            <p>Python</p>
+          </div>
+          <div className={styles.logo}>
             <img src={csharp} alt="C# Logo" />
             <p>C#</p>
+          </div>
+          <div className={styles.logo}>
+            <img src={sql} alt="SQL Logo" />
+            <p>SQL</p>
           </div>
         </div>
       </div>
@@ -82,6 +101,27 @@ function Competences() {
               <p>MacOS</p>
             </div>
           </div>
+      </div>
+      <div className={styles.outilsContainer}>
+        <h2>Outils</h2>
+        <div className={styles.outils}>
+          <div className={styles.outil}>
+            <img src={vscode} alt="VSCode Logo" />
+            <p>VSCode</p>
+          </div>
+          <div className={styles.outil}>
+            <img src={postman} alt="Postman Logo" />
+            <p>Postman</p>
+          </div>
+          <div className={styles.outil}>
+            <img src={github} alt="GitHub Logo" />
+            <p>GitHub</p>
+          </div>
+          <div className={styles.outil}>
+            <img src={cisco} alt="Cisco Logo" />
+            <p>Cisco Packet Tracer</p>
+          </div> 
+        </div>
       </div>
     </div>
   )
