76cd29c661773c2e6b6d06fc9d3a35b6d81cb361
XxxAS7xxX <axel.delpierre78@gmail.com>
2026-01-03 17:24:53 +0000

    profil fini

--- src/profil.jsx
+++ src/profil.jsx
@@ -1,26 +1,34 @@
 import styles from './styles/profil.module.css';
-// import photoProfil from './assets/photoProfil.jpg';
+import photoProfil from './assets/portraitphoto.avif';
 import useOverflow from './useOverflow';
 
+
 const Profil = () => {
-  useOverflow('scroll');
+  useOverflow('hidden');
   return (
     <div className={styles.profil}>
-      <h1>À propos de moi</h1>
-      <div className={styles.content}>
-        {/* <img src={photoProfil} alt="Photo de Profil" className={styles.photo} /> */}
-        <div className={styles.description}>
-          <p>
-            Bonjour! Je m'appelle Axel DELPIERRE, un étudiant passionné en 2ème année de BTS SIO au lycée Jean Vilar, spécialisé en SLAM (Solutions Logicielles et Applications Métiers). Mon parcours académique m'a permis d'acquérir des compétences solides en développement logiciel, gestion de projets informatiques et analyse des besoins métiers.
-          </p>
-          <p>
-            En dehors de mes études, je suis un fervent amateur de nouvelles technologies et je consacre une partie de mon temps libre à explorer les dernières tendances en matière de développement web et mobile. J'aime également participer à des hackathons et des projets open source pour mettre en pratique mes compétences et collaborer avec d'autres passionnés.
-          </p>
-          <p>
-            Mon objectif est de devenir un développeur compétent et polyvalent, capable de concevoir des solutions innovantes qui répondent aux besoins des utilisateurs. Je suis toujours à la recherche de nouvelles opportunités pour apprendre et grandir dans le domaine de l'informatique.
-          </p>
+        <h1>À propos de moi</h1>
+        <div className={styles.content}>
+            <img src={photoProfil} alt="Photo de Profil" className={styles.photo} />
+            <div className={styles.description}>
+                <p>
+                    Je m'appelle Axel DELPIERRE, étudiant en 2ème année de BTS SIO au lycée Jean Vilar, 
+                    spécialisé en SLAM (Solutions Logicielles et Applications Métiers). 
+                    Passioné par l'innovation et l'originalité, je suis constamment à la recherche de nouvelles idées
+                    ou de nouveaux concepts. Je suis également très curieux et j'aime explorer différents domaines 
+                    pour élargir mes connaissances et compétences.
+                </p>
+                <br />
+                <p>
+                    Mon objectif est d’acquérir un maximum de compétences dans différents domaines 
+                    afin de pouvoir m’adapter à toutes les situations et répondre efficacement à toute éventualité. 
+                    Cette polyvalence me permettra d’élargir mes capacités, 
+                    de mieux comprendre des problématiques variées et d’apporter des solutions pertinentes, 
+                    quelles que soient les circonstances.
+                </p>
+            </div>
         </div>
-      </div>
+        <a href="/cv/cv.pdf" target="_blank" rel="noopener noreferrer" className={styles.btnCv}>Télécharger mon CV</a>
     </div>
   );
 };
